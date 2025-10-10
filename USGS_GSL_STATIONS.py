f49afc219664e5b4bebed603b225fc098dea515d
Jacob Lipscomb <jrlipscomb256@gmail.com>
2025-10-09 23:16:28 -0600

    sucked today

--- USGS_GSL_STATIONS.py
+++ USGS_GSL_STATIONS.py
@@ -1,8 +1,15 @@
 USGS_GSL_STATIONS = [
+    # Just GSL wasn't sufficient need more shoreline
     "USGS-10010027",
     "USGS-10010024",
     "USGS-10010100",
     "USGS-10010050",
     "USGS-10141000",
-    "USGS-10010000"
+    "USGS-10010000",
+    # Shoreline (? nearby at least)
+    "USGS-414411112543701",
+    "USGS-414236112101201",
+    "USGS-411348112013601",
+    "USGS-411322112001101",
+    ""
 ]
